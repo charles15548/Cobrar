0887c10b50c3f6f9e28fc1051ce7e027f61b4a78
Charles <charleshuamanllaccta@gmail.com>
2025-02-25 15:48:36 -0500

    commit 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
-import 'screens/clientes_screen.dart';
-import 'database/sqlite_handler.dart';
+import 'src/screens/clientes_screen.dart';
+import 'src/database/sqlite_handler.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-   await SqliteHandler.instance.database;
+  print("🔥 Iniciando la app...");
+  
+  await SqliteHandler.instance.database;
+  
+  print("✅ Base de datos inicializada.");
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    print("🎯 Construyendo MyApp...");
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Gestión de Pagos',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      title: 'Cobra App',
       home: ClientesScreen(),
     );
   }
